74024a8961cea1d1e95902253b8bbc78ba6ad916
F1NH4WK <araujoplayer@gmail.com>
2022-07-22 14:41:45 -0300

    please work

---
+++ App.js
@@ -0,0 +1,22 @@
+import React, {useState, useEffect} from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { Ionicons } from '@expo/vector-icons'; 
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Gay</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
